6ad5ccc69abe979aad59f41c6c25bf402a614ab8
Joel Heiskanen <jopeh99@gmail.com>
2025-04-13 23:22:52 +0300

    Adding starting files to the repository.

---
+++ src/main/java/com/example/java_project/HelloApplication.java
@@ -0,0 +1,82 @@
+package com.example.java_project;
+
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.*;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+// alustava sopimus esitutkimuksesta
+// alustava projektisuunnitelma esitutkimuksesta
+// dokumentointisuunnitelma esitutkimuksesta
+// mikä tahansa suunnitelma määrittelystä
+
+
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+
+            /// Alustava kysely
+            String sql = "SELECT email FROM users WHERE id < 3;";
+
+            Properties prop = new Properties();
+
+
+            //Lataa tiedot db.properties tiedostosta joka on oma kaikille käyttäjille
+            try(InputStream input = new FileInputStream("db.properties")){
+                prop.load(input);
+            }
+
+            /// Asetetaan eri "db.properties" tiedostoon asetetut tiedot anetuihin muutujiin
+            String url = prop.getProperty("db.url");
+            String username = prop.getProperty("db.username");
+            String password = prop.getProperty("db.password");
+
+
+            /// Luodaan yhteys databaseen anetuilla tiedoilla
+            Connection con = DriverManager.getConnection(url, username, password);
+            /// Aluetetaan lysely
+            Statement st = con.createStatement();
+            /// Haetaan kyselyn vastaukset anetulla lauseella
+            ResultSet rs = st.executeQuery(sql);
+
+            //Käydään rivehä niin pitkän kunnes kaikki haetut arviot on käyty läpi
+            //Eka rivi tulee skipata koska siinäö ei ole arvoja jota haetaan taulusta
+            while(rs.next()){
+                String word = rs.getString(1);
+                System.out.println(word);
+            }
+
+        }catch (SQLException E){
+            System.out.println("Exeption: " + E);
+        }
+
+
+
+
+        /*
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+        */
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
